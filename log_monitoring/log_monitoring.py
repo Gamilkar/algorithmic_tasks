4a3e40b54b37b6cb3ce397a82543c678c45d4369
Gamilkar <gamilkarr@gmail.com>
2022-01-20 12:57:23 +0300

    fix 'log_monitoring'

--- log_monitoring/log_monitoring.py
+++ log_monitoring/log_monitoring.py
@@ -1,21 +1,26 @@
 import datetime
 
-result = []
-with open("test_3.txt", "r") as log_file:
-    line = log_file.readline().split()
-    t, e = list(map(int, line))
+
+def log_monitoring(t, e, log):
     interval = datetime.timedelta(seconds=t)
-    while True:
-        line = log_file.readline()
-        if not line:
-            print(-1)
-            break
+    result = -1
+    errors = []
+    for line in log:
         status = line[22:27]
         if status == "ERROR":
             date = datetime.datetime.strptime(line[1:20], "%Y-%m-%d %H:%M:%S")
-            result.append(date)
-            while result[-1] >= result[0] + interval:
-                result.pop(0)
-            if len(result) >= e:
-                print(str(date))
-                break
+            errors.append(date)
+            if len(errors) == e:
+                if errors[-1] < errors[0] + interval:
+                    result = date
+                    break
+                errors.pop(0)
+    return str(result)
+
+
+with open("test_2.txt", "r") as log_file:
+    t, e = list(map(int, log_file.readline().split()))
+    log = log_file.read().split("\n")
+
+
+print(log_monitoring(t, e, log))
